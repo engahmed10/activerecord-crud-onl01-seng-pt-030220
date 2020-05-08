816ec01a6e981eb5958b61a07ac79403a2489801
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 19:42:07 +0000

    Automatically backed up by Learn

--- db/migrate/20200508193928_create_movies.rb
+++ db/migrate/20200508193928_create_movies.rb
@@ -1,6 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-
+   
     
   end
 end
+
+
+Movie.create(title: "Wargames", release_date: 1983, director: "John Badham", lead: "Matthew Broderick", in_theaters: false)
