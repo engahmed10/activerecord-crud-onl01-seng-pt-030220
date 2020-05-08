4c6ecfdb643f2151987658341e9ce56f4e8d01a5
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 19:45:09 +0000

    Automatically backed up by Learn

--- db/migrate/20200508193928_create_movies.rb
+++ db/migrate/20200508193928_create_movies.rb
@@ -1,6 +1,12 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-   
+   create_table craete_movies do |t|
+     t.string :title
+     t.integer :release_date
+     t.sting :director
+     t.string :lead
+     t.boolean :in_theaters
+   end
 
   end
 end
