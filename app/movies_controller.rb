416a4df8b6faf0893244afcb6957eb15eef147c3
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 20:57:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone",release_date: 1990})
 
   Movie.create do |m|
      m[1] = args[:title]
-     m[2] =args[:release_date]
+     m[2] = args[:release_date]
      binding.pry
 
   end
