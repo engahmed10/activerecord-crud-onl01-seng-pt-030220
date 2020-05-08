282b6d8ef93e5ef7e200a63e41437f0b6fc4fe3b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 22:14:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,10 +62,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.find_or_create_by(first_name: 'Andy') do |c|
-    c.release_date > 2002
-  end
-  
+  Movie.where(release_date > 2002).order(:release_date)
+
 end
 
 def can_be_found_updated_and_saved
